494d9b7955f091d2a5fbbea75346f3e2537d80e4
Radek Novotny <rnovotny@c3r9s1.42prague.com>
2023-01-16 13:24:33 +0100

    Added ft_putendl_fd.c, ft_putnbr_fd.c

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 13:14:18 by rnovotny          #+#    #+#             */
+/*   Updated: 2023/01/16 13:23:39 by rnovotny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+static void	ft_special(int n, int fd)
+{
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	if (n == 0)
+		write(fd, "0", 1);
+}
+
+void ft_putnbr_fd(int n, int fd)
+{
+	char	buf[10];
+	int		i;
+	int		neg;
+
+	i = 0;
+	neg = 0;
+	ft_special(n, fd);
+	if (n < 0 && n != -2147483648)
+	{
+		n = -n;
+		neg = 1;
+	}
+	while (n > 0)
+	{
+		buf[i] = n % 10 + 48;
+		n = n / 10;
+		i++;
+	}
+	if (neg)
+		write(fd, "-", 1);
+	while (i > 0)
+	{
+		i--;
+		write(fd, &buf[i], 1);
+	}
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 12:05:50 by rnovotny          #+#    #+#             */
-/*   Updated: 2023/01/16 13:08:03 by rnovotny         ###   ########.fr       */
+/*   Updated: 2023/01/16 13:14:55 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,6 @@ void ft_striteri(char *s, void (*f)(unsigned int, char*));
 void ft_putchar_fd(char c, int fd);
 void ft_putstr_fd(char *s, int fd);
 void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
 
 #endif
